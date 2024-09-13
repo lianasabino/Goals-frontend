4e71ba9096465a5ccdc3d6377ca78c8d66c5d00f
lianasabino <lianasabino@hotmail.com>
2024-09-13 16:24:24 -0300

    connect with backend

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,33 @@
 import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
 import { Summary } from "./components/summary";
-
-// import { EmptyGoals } from "./components/empty-goals";
+import { EmptyGoals } from "./components/empty-goals";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "./http/get-summary";
 
 export function App() {
+  // const [summary, setSummary] = useState<SummaryResponse | null>(null);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3333/summary")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       setSummary(data.summary);
+  //     });
+  // }, []);
+
+  const { data } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, //60 segundos
+  });
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
       <CreateGoal />
     </Dialog>
   );
--- src/components/empty-goals.tsx
+++ src/components/empty-goals.tsx
@@ -1,17 +1,17 @@
 import logo from "../assets/logo-in-orbit.svg";
 import letsStart from "../assets/lets-start-illustration.svg";
+
 import { DialogTrigger } from "./ui/dialog";
-import { Plus } from "lucide-react";
 import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
 
 export function EmptyGoals() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-5">
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
       <img src={logo} alt="in.orbit" />
       <img src={letsStart} alt="in.orbit" />
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
-        mesmo?
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
 
       <DialogTrigger asChild>
